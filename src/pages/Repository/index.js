45b207184680ba8ca8fe772c8bce368e4cd30a35
Alley M. Carvalho <alleycarvalho@gmail.com>
2019-11-07 13:03:56 -0300

    Loading data from api

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+import Container from '../../components/Container';
+import { Loading, Owner } from './styles';
+
+class Repository extends Component {
+  state = {
+    repository: {},
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const repository = await api.get(`/repos/${repoName}`);
+
+    this.setState({
+      repository: repository.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, loading } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+      </Container>
+    );
+  }
 }
+
+export default Repository;
